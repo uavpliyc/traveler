aa1f51be0fa9a5afc9a4e72023a9960abe2fcea2
uavpliyc <uavpliyc@gmail.com>
2021-05-14 22:58:00 +0900

    add favorite

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root :to => 'homes#top'
   resources :posts do
     resources :comments, only:[:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
 
 end
