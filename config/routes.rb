347c0f94ab0604a2778ef3a9da5e2072f86d2cf2
uavpliyc <uavpliyc@gmail.com>
2021-05-14 23:35:36 +0900

    add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'homes#top'
@@ -6,5 +8,6 @@ Rails.application.routes.draw do
     resources :comments, only:[:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+  resources :users, only:[:index, :show]
 
 end
