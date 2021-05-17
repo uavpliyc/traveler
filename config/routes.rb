1878906548808293f2357295d5312d57f11a34e3
uavpliyc <uavpliyc@gmail.com>
2021-05-17 15:06:02 +0900

    add follow routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     resources :comments, only:[:create, :destroy]
     resource :favorites, only:[:create, :destroy]
   end
-  resources :users, only:[:index, :show, :edit, :update]
+  resources :users, only:[:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+  end
 
 end
