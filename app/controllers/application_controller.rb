695a0315db980f4b2af312e9edc6ff48eb1ae09d
uavpliyc <uavpliyc@gmail.com>
2021-05-18 14:31:16 +0900

    fix after_sign_in_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
+  def after_sign_in_path_for(resource)
+    posts_path
+  end
+
 end
