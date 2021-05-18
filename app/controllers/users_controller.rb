12db3b655390dbb9e9e556ba10c141f9b39c7f69
uavpliyc <uavpliyc@gmail.com>
2021-05-18 13:03:40 +0900

    fix follow function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.page(params[:page]).reverse_order
-    @following_users = current_user.following_user
-    @follower_users = current_user.follower_user
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
   end
 
   def edit
@@ -22,12 +22,12 @@ class UsersController < ApplicationController
 
   def follows
     user = User.find(params[:id])
-    @users = user.follower.page(params[:page]).reverse_order
+    @users = user.following_user.page(params[:page]).reverse_order
   end
 
   def followers
     user = User.find(params[:id])
-    @users = user.followed.page(params[:page]).reverse_order
+    @users = user.follower_user.page(params[:page]).reverse_order
   end
 
   private
